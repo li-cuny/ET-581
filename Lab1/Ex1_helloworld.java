85b3a245f6748952589bbe1b4d96038ef2b25703
li-cuny <changlin.li@qcc.cuny.edu>
2025-08-27 00:30:40 +0000

    save Hello world

---
+++ Lab1/Ex1_helloworld.java
@@ -0,0 +1,9 @@
+package Lab1;
+
+public class Ex1_helloworld {
+    public static void main(String[] args) {
+        System.out.print("this is not new line");
+        System.out.println("Hello World!");
+        System.out.print("this is not new line");
+    }
+}
