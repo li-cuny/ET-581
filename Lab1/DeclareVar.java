784708cc68cb7526a78ee14501b5d1542ce0fe1e
li-cuny <changlin.li@qcc.cuny.edu>
2025-08-28 23:24:03 +0000

    declearVar.java

---
+++ Lab1/DeclareVar.java
@@ -0,0 +1,9 @@
+public class DeclareVar {
+    public static void main(String[] args){
+        // this is printing char at 3rd place
+        String name = "Changlin Li";
+        System.out.println(name.charAt(2));   // this is print 
+        
+
+    }
+}
